a6df1bc5a8505514744a3447cec19154ca332e55
Tik Tok Api <webqart@icloud.com>
2022-02-05 15:10:41 +0300

    Create bot-start.php

---
+++ bot-start.php
@@ -0,0 +1,13 @@
+<?php
+require('classInstagram.php');
+@set_time_limit(0);
+@clearstatcache();
+@ini_set('max_execution_time',0);
+@ini_set('output_buffering',0);
+echo "Berapa Banyak Akun Yang Mau dibuat[?]:";
+$count = trim(fgets(STDIN, 1024));
+echo "Delaynya Mau Berapa Detik [?]:";
+$sleep = trim(fgets(STDIN, 1024));
+echo "[!] Tunggu gan. . .\n\n";
+$i = new instaCreator();
+$i->userCreate($count,$sleep);
